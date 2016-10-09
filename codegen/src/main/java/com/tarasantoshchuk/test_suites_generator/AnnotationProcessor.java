1448f2f32bcafd390abf4702c00fc63914bb0419
tarasantoshchuk <t.antoshchuk@gmail.com>
2016-10-10 00:49:09 +0300

    AnnotationProcessor refactoring

--- codegen/src/main/java/com/tarasantoshchuk/test_suites_generator/AnnotationProcessor.java
+++ codegen/src/main/java/com/tarasantoshchuk/test_suites_generator/AnnotationProcessor.java
@@ -7,6 +7,7 @@ import com.tarasantoshchuk.test_suites_generator.model.SuiteType;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashSet;
@@ -18,6 +19,8 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 
 public class AnnotationProcessor extends AbstractProcessor {
@@ -53,27 +56,33 @@ public class AnnotationProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        final ArrayList<AnnotatedClass> list = new ArrayList<>();
 
-        ArrayList<AnnotatedClass> list = new ArrayList<>();
-        for (Element annotatedElement : roundEnvironment.getElementsAnnotatedWith(UnitTest.class)) {
-            list.add(
-                    new AnnotatedClass(
-                            annotatedElement.getAnnotation(UnitTest.class).suiteName(),
-                            SuiteType.UNIT_TESTS,
-                            (TypeElement) annotatedElement
-                    )
-            );
-        }
+        traceAnnotatedClasses(roundEnvironment, UnitTest.class, new AnnotatedClassIterator<UnitTest>() {
+            @Override
+            public void onAnnotatedClass(TypeElement annotatedElement, UnitTest annotation) {
+                list.add(
+                        new AnnotatedClass(
+                                annotation.suiteName(),
+                                SuiteType.forAnnotation(UnitTest.class),
+                                annotatedElement
+                        )
+                );
+            }
+        });
 
-        for (Element annotatedElement : roundEnvironment.getElementsAnnotatedWith(UiTest.class)) {
-            list.add(
-                    new AnnotatedClass(
-                            annotatedElement.getAnnotation(UiTest.class).suiteName(),
-                            SuiteType.UI_TESTS,
-                            (TypeElement) annotatedElement
-                    )
-            );
-        }
+        traceAnnotatedClasses(roundEnvironment, UiTest.class, new AnnotatedClassIterator<UiTest>() {
+            @Override
+            public void onAnnotatedClass(TypeElement annotatedElement, UiTest annotation) {
+                list.add(
+                        new AnnotatedClass(
+                                annotation.suiteName(),
+                                SuiteType.forAnnotation(UiTest.class),
+                                annotatedElement
+                        )
+                );
+            }
+        });
 
         for (Element annotatedElement : roundEnvironment.getElementsAnnotatedWith(BrokenTest.class)) {
             BrokenTest brokenTest = annotatedElement.getAnnotation(BrokenTest.class);
@@ -97,6 +106,11 @@ public class AnnotationProcessor extends AbstractProcessor {
 
         Collection<SuiteModel> suites = SuiteModel.generateSuiteModelClasses(list);
 
+        createSuiteClasses(suites);
+        return true;
+    }
+
+    private void createSuiteClasses(Collection<SuiteModel> suites) {
         for (SuiteModel suite: suites) {
             Writer writer;
             try {
@@ -110,6 +124,33 @@ public class AnnotationProcessor extends AbstractProcessor {
                 mMessager.warn(e.getMessage(), null);
             }
         }
-        return true;
+    }
+
+    private <T extends Annotation> void traceAnnotatedClasses(RoundEnvironment roundEnvironment, Class<T> annotationClass, AnnotatedClassIterator<T> iterator) {
+        for (Element element: roundEnvironment.getElementsAnnotatedWith(annotationClass)) {
+            TypeElement typeElement = getValidTypeElement(element);
+            T annotation = element.getAnnotation(annotationClass);
+
+            iterator.onAnnotatedClass(typeElement, annotation);
+        }
+    }
+
+    private TypeElement getValidTypeElement(Element element) {
+        if (element instanceof TypeElement && (element.getKind() == ElementKind.CLASS)) {
+            TypeElement typeElement = (TypeElement) element;
+
+            if (!typeElement.getModifiers().contains(Modifier.PUBLIC)) {
+                mMessager.error("Annotated element must be public", element);
+            }
+
+            return typeElement;
+        } else {
+            mMessager.error("Annotated element is not a class", element);
+            return null;
+        }
+    }
+
+    private interface AnnotatedClassIterator<T> {
+        void onAnnotatedClass(TypeElement annotatedElement, T annotation);
     }
 }
--- codegen/src/main/java/com/tarasantoshchuk/test_suites_generator/model/SuiteType.java
+++ codegen/src/main/java/com/tarasantoshchuk/test_suites_generator/model/SuiteType.java
@@ -1,5 +1,10 @@
 package com.tarasantoshchuk.test_suites_generator.model;
 
+import com.tarasantoshchuk.test_suites_generator.UiTest;
+import com.tarasantoshchuk.test_suites_generator.UnitTest;
+
+import java.lang.annotation.Annotation;
+
 public enum SuiteType {
     UNIT_TESTS("UnitTest"),
     UI_TESTS("UiTest");
@@ -11,5 +16,15 @@ public enum SuiteType {
 
     public String nameSuffix() {
         return mNameSuffix;
+    }
+
+    public static SuiteType forAnnotation(Class<? extends Annotation> annotation) {
+        if (annotation == UnitTest.class) {
+            return UNIT_TESTS;
+        } else if (annotation == UiTest.class) {
+            return UI_TESTS;
+        } else {
+            throw new RuntimeException("No suite type for annotation " + annotation.getName());
+        }
     }
 }
